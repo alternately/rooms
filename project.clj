77dc0ddaa26e2c80b43bf4a4fd3da9b6c6d0588c
Alternately <apgiuffre@gmail.com>
2019-01-14 00:13:32 -0500

    working on writing rooms

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject rooms "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A little text based adventure platform. Not many features (yet :) )"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
